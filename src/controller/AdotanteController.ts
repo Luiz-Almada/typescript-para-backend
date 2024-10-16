2b5783c85c0fef07abffdc76c58382a1b43dbae4
Luiz Almada <luizalmada@yahoo.com>
2024-10-16 01:34:31 -0300

    Criando um validador de Adotante - Part 02

--- src/controller/AdotanteController.ts
+++ src/controller/AdotanteController.ts
@@ -3,12 +3,21 @@ import { Request, Response } from "express";
 import AdotanteEntity from "../entities/AdotanteEntity";
 import AdotanteRepository from "../repositories/AdotanteRepository";
 import EnderecoEntity from "../entities/EnderecoEntity";
+import * as yup from "yup";
 import {
   TipoRequestBodyAdotante,
   TipoRequestParamsAdotante,
   TipoResponseBodyAdotante,
 } from "../tipos/tiposAdotante";
 
+const adotanteBodyValidator: yup.ObjectSchema<
+  Omit<TipoRequestBodyAdotante, "endereco">
+> = yup.object({
+  nome: yup.string().defined().required(),
+  celular: yup.string().defined().required(),
+  senha: yup.string().defined().required().min(6),
+  foto: yup.string().optional(),
+});
 export default class AdontanteController {
   constructor(private repository: AdotanteRepository) {}
   async criaAdotante(
@@ -17,6 +26,25 @@ export default class AdontanteController {
   ) {
     try {
       const { nome, celular, endereco, foto, senha } = <AdotanteEntity>req.body;
+
+      let bodyValidated: TipoRequestBodyAdotante;
+      try {
+        bodyValidated = await adotanteBodyValidator.validate(req.body, {
+          abortEarly: false,
+        });
+      } catch (error) {
+        const yupErrors = error as yup.ValidationError;
+
+        // Record<chave, valor> é um objeto do Typescript
+        const validationErrors: Record<string, string> = {};
+
+        yupErrors.inner.forEach((error) => {
+          if (!error.path) return;
+          validationErrors[error.path] = error.message;
+        });
+        //return res.status(404).json({ error: yupErrors.message });
+        return res.status(404).json({ error: validationErrors });
+      }
 
       const novoAdotante = new AdotanteEntity(
         nome,
@@ -31,7 +59,7 @@ export default class AdontanteController {
         .status(201)
         .json({ data: { id: novoAdotante.id, nome, celular } });
     } catch (error) {
-      return; // res.status(500).json({ error: "Erro ao criar o adotante" });
+      return res.status(500).json({ error: "Erro ao criar o adotante" });
     }
   }
 
